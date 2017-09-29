ce9e3b86b36708e3bea19d15d2c6515807464698
JORGE-DANILO <JORGE-DANILO@JORGE-DANILO-PC>
2017-09-29 10:22:40 -0300

    fim

---
+++ src/Principal.java
@@ -0,0 +1,22 @@
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Aluno a1 = new Aluno("Jorge", "21603818", 28, "Ciencia da Computação", "noturno", "Masculino");
+		Disciplina d1 = new Disciplina("Linguagem de programação", "Ciencia da computação", 90);
+		Disciplina d2 = new Disciplina("Estrutura de dados", "Ciencia da Computação", 75);
+
+		System.out.println("<<< Dados do Aluno >>>" + a1);
+		System.out.println("\n<<< Diciplinas 1 >>>" + d1 + "\n\n<<< Diciplina 2 >>>" + d2);
+
+		System.out.println(a1.imprimirAluno());
+
+		System.out.println(a1.getNome());
+
+		System.out.println(a1.getRA());
+
+	}
+
+}
